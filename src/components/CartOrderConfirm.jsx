e046bb565cadfb1bd9f3459a4d4db221b57f3902
prawit2101254411 <162869085+prawit2101254411@users.noreply.github.com>
2024-07-08 18:05:09 +0700

    basketData to confirmed

---
+++ src/components/CartOrderConfirm.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function CartOrderConfirm({title,amount}) {
+  return (
+    <div className="flex w-[800px] min-w-[400px] p-1">
+      <div className="avatar">
+        <div className="w-24 rounded-full">
+          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+      <div className=" w-[100%] p-2">
+        <h1>{title}</h1>
+        <p className=" text-gray-400 font-mono">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
+          expedita{" "}
+        </p>
+      </div>
+      <div className=" flex gap-3 justify-end items-center w-[200px] h-[100px] ">
+        THB {amount}
+      </div>
+    </div>
+  )
+}
