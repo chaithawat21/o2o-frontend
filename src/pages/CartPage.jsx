e927bbfe6841ac5abe738d0eef71a1a97e4672bc
prawit2101254411 <162869085+prawit2101254411@users.noreply.github.com>
2024-07-09 18:11:55 +0700

    resAPI payment using stripe

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -3,54 +3,52 @@ import CartList from "../components/CartList";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
-import { CloudFog } from "lucide-react";
+import { useUser } from "../utils/serviceAPI/backendService-zustend";
 import axios from "axios";
 
 function CartPage() {
-  const [loan, setLoan] = useState([]);
-  // console.log(loan)
+  const user = useUser((stete) => stete.user)
+  const lend = useUser((stete) => stete.lendUser)
 
+  const fectUser = useUser((stete) => stete.fectDataUser)
+  const fectLendById = useUser((stete) => stete.fectLendById)
+  
+  // console.log(lend)
   useEffect(()=> {
-    const run = async () => {
-      try{ 
-        const rs = await axios.get("http://localhost:8585/loan")
-        setLoan(rs.data)
-        }catch(err){
-          console.log(err)
-        }
-    }
-    run()
-  },[])
+    fectUser()
+    fectLendById()
+  },[lend])
 
-  const handleDelete = (index) => {
-    const newLoan = loan.filter((_, i) => i !== index);
-    setLoan(newLoan);
+  const handleDelete = (id) => {
+    console.log(id)
+
   };
 
-  const handleAmountChange =async (title, newAmount) => {
-    setLoan((prevLoan) =>
-      prevLoan.map((item) =>
-        item.title === title ? { ...item, amount: newAmount } : item
-      )
-    );
+  const handleAmountChange =async (id, newAmount) => {
+    const body = {id,newAmount}
+    const rs = await axios.put("http://localhost:8888/lend",body,{
+      headers: { Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZmlyc3RuYW1lIjoiQW5keSIsImlhdCI6MTcyMDUxMTY0MSwiZXhwIjoxNzIzMTAzNjQxfQ.hJOvNZLYc_t9tL_xcKNw4MHhJ0dhwCMakDZmm_UeXQY" }
+    })
+    console.log(rs.data)
   };
 
-  const chackloan = loan.length === 0 ? 
+  const chackloan = lend.length === 0 ? 
   <p>No orders</p> 
   :
-  loan.map((item, index) => (
+  lend.map((item, index) => (
     <CartList
       key={index}
-      index={index}
-      title={item.title}
-      amount={item.amount}
-      setAmount={(newAmount) => handleAmountChange(item.title, newAmount)}
+      id={item.id}
+      title={item?.loan?.categorie}
+      story={item?.loan?.story}
+      amount={item?.amount}
+      setAmount={(newAmount) => handleAmountChange(item.id, newAmount)}
       handleDelete={handleDelete}
     />
   ))
 
 
-  const totalAmount = loan.reduce((sum, item) => sum + item.amount, 0);
+  const totalAmount = lend.reduce((sum, item) => sum + item.amount, 0);
   return (
     <div className="flex justify-center p-10">
       <div className="flex gap-2 items-center flex-col w-[70%]">
@@ -63,7 +61,7 @@ function CartPage() {
             Total THB {totalAmount}
             <div className="flex gap-4">
               <button className="btn btn-outline">Payment register</button>
-              <Link to={`/orderconfirm?loan=${encodeURIComponent(JSON.stringify(loan))}`}>
+              <Link to={`/orderconfirm?lend=${encodeURIComponent(JSON.stringify(lend))}`}>
                 <button className="btn btn-accent">Continue</button>
               </Link>
             </div>
--- src/utils/serviceAPI/backendService-zustend.js
+++ src/utils/serviceAPI/backendService-zustend.js
@@ -1,2 +1,33 @@
+import axios from "axios";
 import { create } from "zustand";
 
+const dataUser = (set, get) => ({
+    loading: false,
+    user: [],
+    lendUser: [],
+    fectDataUser: async () => {
+        try {
+            set(stete => ({ ...stete, loading: true }))
+            const rs = await axios.get("http://localhost:8888/auth/me", {
+                headers: { Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZmlyc3RuYW1lIjoiQW5keSIsImlhdCI6MTcyMDUxMzAyNywiZXhwIjoxNzIzMTA1MDI3fQ.Mkqf6UwMXYanxpkJPOvcMTkhHXQyzuTxSTOVu1TDvc8" }
+            })
+            set( state => ({...state, user: rs.data.user}))
+        } catch (err) {
+            console.log(err)
+        }
+    },
+    fectLendById: async () => {
+        try {
+            set(stete => ({ ...stete, loading: true }))
+            const rs = await axios.get("http://localhost:8888/lend", {
+                headers: { Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZmlyc3RuYW1lIjoiQW5keSIsImlhdCI6MTcyMDUxMzAyNywiZXhwIjoxNzIzMTA1MDI3fQ.Mkqf6UwMXYanxpkJPOvcMTkhHXQyzuTxSTOVu1TDvc8" }
+            })
+            set( state => ({...state, lendUser: rs.data}))
+        } catch (err) {
+            console.log(err)
+        }
+    }
+})
+
+const useUser = create(dataUser)
+export {useUser}
