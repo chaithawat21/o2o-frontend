a01a6ffa6793b79b279485929ea44031cdc2a299
chai <chaithawat.contact@gmail.com>
2024-07-10 16:12:38 +0700

    update homepage and add framer motion

--- src/components/FooterHome.jsx
+++ src/components/FooterHome.jsx
@@ -4,11 +4,11 @@ import ImageFooter from '../assets/images/illustration/illustration02-footer.svg
 
 function FooterHome() {
   return (
-    <div className='h-[15rem] bg-GreenFooter flex flex-col justify-center items-center gap-[1rem]'>
+    <div className='h-[18rem] bg-GreenFooter flex flex-col justify-center items-center gap-[1rem]'>
         <img className='w-[6rem]' src={ImageFooter} alt="image-footer" />
         <p className='text-white text-[1.5rem]'>เริ่มแบ่งปันได้แล้ววันนี้</p>
         <Link to="/select">
-        <button className='bg-white text-[.6rem] px-[1rem] py-[.5rem] rounded-[10px]'>เลือกคนที่จะแบ่งปันรอยยิ้ม</button>
+        <button className='bg-white text-[1.25rem] px-[1rem] py-[.5rem] rounded-[10px]'>เลือกคนที่จะแบ่งปันรอยยิ้ม</button>
         </Link>
     </div>
   )
