7a11dbca47bfcc4e6aa4994a43fe64b0b3200cdc
Aphiwat <aphiwat.jantharasrisao@gmail.com>
2024-07-15 16:54:17 +0700

    searchbar pagebyId ok

--- src/utils/serviceAPI/searchServices.js
+++ src/utils/serviceAPI/searchServices.js
@@ -2,95 +2,108 @@ import axios from "axios";
 import { create } from "zustand";
 
 const SearchData = (set, get) => ({
-  loading : false,
+  loading: false,
   searchInfo: [],
   fillterProvinceValue: [],
-  inputCheck : false,
-  loanData:[],
-  regionData : [],
-  LoanDataById : [],
+  inputCheck: false,
+  loanData: [],
+  regionData: [],
+  LoanDataById: [],
 
   fetchSearchData: async () => {
     try {
-      set(state => ({...state, loading:true}))
+      set((state) => ({ ...state, loading: true }));
       const getSearch = await axios.get("http://localhost:8888/search/type");
       set((state) => ({ ...state, searchInfo: getSearch.data }));
     } catch (err) {
       console.log(err.message);
-    }finally {
-			set(state => ({...state , loading: false}))		
-		}
+    } finally {
+      set((state) => ({ ...state, loading: false }));
+    }
   },
 
-
-  FillterProvince: (value,province) => {
-    const rs = province.filter(
-      (province) => province.region_id === value
-    );
+  FillterProvince: (value, province) => {
+    const rs = province.filter((province) => province.region_id === value);
     set((state) => ({ ...state, fillterProvinceValue: rs }));
   },
 
-  checkValue : (value) => {
-    value && set((state)=> ({...state,inputCheck :true }))
-    !value && set((state)=> ({...state,inputCheck :false }))
+  checkValue: (value) => {
+    value && set((state) => ({ ...state, inputCheck: true }));
+    !value && set((state) => ({ ...state, inputCheck: false }));
   },
 
   fetchLoanData: async () => {
-
-    try{
-      set(state => ({...state, loading:true}))
-      const getLoanData = await axios.get("http://localhost:8888/loan/getloan")
+    try {
+      set((state) => ({ ...state, loading: true }));
+      const getLoanData = await axios.get("http://localhost:8888/loan/getloan");
       set((state) => ({ ...state, loanData: getLoanData.data }));
     } catch (err) {
       console.log(err.message);
-    }finally {
-			set(state => ({...state , loading: false}))		
-		}
+    } finally {
+      set((state) => ({ ...state, loading: false }));
+    }
   },
 
-  fetchLoanDataById : async (id) =>{
-    try{
-      set(state => ({...state, loading:true}))
-      const getLoanDataById = await axios.get(`http://localhost:8888/loan/getLoanById/${id}`)
+  fetchLoanDataById: async (id) => {
+    try {
+      set((state) => ({ ...state, loading: true }));
+      const getLoanDataById = await axios.get(
+        `http://localhost:8888/loan/getLoanById/${id}`
+      );
       set((state) => ({ ...state, LoanDataById: getLoanDataById.data }));
     } catch (err) {
       console.log(err.message);
-    }finally {
-			set(state => ({...state , loading: false}))		
-		}
+    } finally {
+      set((state) => ({ ...state, loading: false }));
+    }
   },
 
-
-  SelectByFillter: async(data,selectType) =>{
-    if(selectType === 'region'){
-      try{
-      set((state) => ({...state, regionData: data }))
-      const getByRegion = await axios.get(`http://localhost:8888/search/region/${data}`)
-      set((state) => ({ ...state, loanData: getByRegion.data }));
-    }catch(err){
-      console.log(err.message);
-    }
-    }
-    if(selectType === 'province'){
-    try{
-      const getByProvince = await axios.get(`http://localhost:8888/search/province/${data}`)
-      set((state) => ({ ...state, loanData: getByProvince.data }));
-    }catch(err){
-      console.log(err.message);
-    }
-    }
-
-     if(selectType === 'categorie'){
-      try{
-        const getCategorie = await axios.get(`http://localhost:8888/search/categorie/${data}`)
-        set((state) => ({ ...state, loanData: getCategorie.data }));
-      }catch(err){
+  SelectByFillter: async (data, selectType) => {
+    if (selectType === "region") {
+      try {
+        set((state) => ({ ...state, regionData: data }));
+        const getByRegion = await axios.get(
+          `http://localhost:8888/search/region/${data}`
+        );
+        set((state) => ({ ...state, loanData: getByRegion.data }));
+      } catch (err) {
         console.log(err.message);
       }
-     }
     }
-  }
-);
+    if (selectType === "province") {
+      try {
+        const getByProvince = await axios.get(
+          `http://localhost:8888/search/province/${data}`
+        );
+        set((state) => ({ ...state, loanData: getByProvince.data }));
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+
+    if (selectType === "categorie") {
+      try {
+        const getCategorie = await axios.get(
+          `http://localhost:8888/search/categorie/${data}`
+        );
+        set((state) => ({ ...state, loanData: getCategorie.data }));
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+    if (selectType === "loan") {
+      try {
+        console.log('test');
+        const getloan = await axios.get(
+          `http://localhost:8888/search/loan/${data}`
+        );
+        set((state) => ({ ...state, loanData: getloan.data }));
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+  },
+});
 
 const useSearchData = create(SearchData);
 export { useSearchData };
